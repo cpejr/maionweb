93f44d11a4160dc64d6589c835cda028d0c672ed
João Pedro Rodrigues Trindade <jpr.trindade@gmail.com>
2019-07-30 12:29:49 -0300

    algumas alterações nos campos interativos

--- public/javascripts/replica.js
+++ public/javascripts/replica.js
@@ -2,10 +2,10 @@ $( "#add-campo-acompanhantes" ).click(function() {
   $( "#add_acompanhantes" ).append( '<select class="form-control" id="select_da_idade"><option selected>Selecione a faixa etária</option><option>Adulto<option/><option>CHD<option/><option>INF<option/><select/>' );
 });
 
-$( "#add-campo-paises" ).click(function() {
-  $( "#location" ).append( '<input type="text" name="" placeholder="Nome do país" class="form-control">' );
+$( ".add-campo-paises" ).click(function() {
+  $( ".location" ).append( '<div class="dinamic_spec"><label>País</label><input type="text" name="budget[location][country]" placeholder="Nome do país" class="form-control"><button type="button" class="add_buttons add-campo-paises"> + Países</button><div class="form-group"><label>Cidade</label><input type="text" name="budget[location][cities]" placeholder="Nome da cidade da viagem" class="form-control"></div><button type="button" class="add_buttons add-campo-cidades"> + Cidades</button></div>' );
 });
 
-$( "#add-campo-cidades" ).click(function() {
-  $( "#location" ).append( '<input type="text" name="" placeholder="Nome da cidade da viagem" class="form-control">' );
+$( ".add-campo-cidades" ).click(function() {
+  $( ".dinamic_spec" ).append( '<input type="text" name="budget[location][cities]" placeholder="Nome da cidade da viagem" class="form-control">' );
 });
