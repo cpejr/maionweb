dd3d8cfae02d8a5fe24367b626a9f2c9e78a8fb7
Camila Murta <camilasilva@cpejr.com.br>
2019-09-06 10:48:02 -0300

    todas as mascaras numericas feitas

--- public/javascripts/formInputs.js
+++ public/javascripts/formInputs.js
@@ -1,4 +1,20 @@
-function addDashes(f) //adicinar parenteses e barra no telefone fixo
+function addDashesFixo(f) //adicinar parenteses e barra no telefone fixo
 {
 f.value = f.value.slice(0,0)+"("+f.value.slice(0,2)+")"+f.value.slice(2,6)+"-"+f.value.slice(6,10);
+}
+function addDashesCel(f) //adicionar parenteses e barra em celular
+{
+f.value = f.value.slice(0,0)+"("+f.value.slice(0,2)+")"+f.value.slice(2,7)+"-"+f.value.slice(7,11);
+}
+function addDashesCPF(f) //adicionar pontos no cpf
+{
+f.value = f.value.slice(0,3)+"."+f.value.slice(3,6)+"."+f.value.slice(6,9)+"-"+f.value.slice(9,11);
+}
+function addDashesCep(f) //adicionar pontos no cep
+{
+f.value = f.value.slice(0,2)+"."+f.value.slice(2,5)+"-"+f.value.slice(5,8);
+}
+function addDashesNascimento(f) //adicionar barras na data de nascimento
+{
+f.value = f.value.slice(0,2)+"/"+f.value.slice(2,4)+"/"+f.value.slice(4,8);
 }
