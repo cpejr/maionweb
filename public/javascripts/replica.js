645c0cb40c14a2ae65455cc6a168bc71360bbc9a
João Pedro Rodrigues Trindade <jpr.trindade@gmail.com>
2019-09-23 11:20:10 -0300

    Page D reformulada
    Page D agora cria seus campos de voo e escalas, sendo q os valores adicionados as passagens conseguem ser tratados em todos os campos + voo rrreplicados

--- public/javascripts/replica.js
+++ public/javascripts/replica.js
@@ -24,32 +24,36 @@ $( "#add_dias" ).click(function() {
   //   $( "#age_acomp" ).append( '<select class="form-control" name="budget[companion][age]"><option selected>Faixa etária do acompanhante</option><option>Adulto</option><option>CHD</option><option>INF</option></select>' );
   // });
 
-var num = 2;
-var day = 1;
-$('#add_country').on('click', function() {
-  $("#button_space").append('<div id="in_country_days'+num+'" class="testeReplicaBox"><div class="days_input"><input placeholder="Nome do país" class="form-control" name="budget[country][]"></input></div>   <div class="days_input"><input placeholder="Aeroporto de ída" class="form-control" name="budget[airportGo][]"></input><input placeholder="Aeroporto de chegada" class="form-control" name="budget[airportReturn][]"></input></div>   <div class="days_input"><input placeholder="data de ída" class="form-control" name="budget[goDate][]"></input><input placeholder="data de chegada" class="form-control" name="budget[returnDate][]"></input></div>   <div class="days_input"><input placeholder="Nome da cidade" class="form-control" name="budget[cities][]"></input></div>   </div>');
-  day = num;
-  num++;
-  $(".testeReplicaBox").removeClass("Selected");
-  $(`#in_country_days${day}`).addClass("Selected");ss
-});
-
-$("#button_day_original").on("click", function(){
-  var newDia = '<div class="days_input"><input placeholder="Nova cidade" class="form-control" name="budget[cities][]"></input></div>';
-  $(`#in_country_days${day}`).append(newDia);
-});
-
-$('#increasing').on('click', function() {
-  day--;
-  $(".testeReplicaBox").removeClass("Selected");
-  $(`#in_country_days${day}`).addClass("Selected");
-});
-
-$('#decreasing').on('click', function() {
-  day++;
-  $(".testeReplicaBox").removeClass("Selected");
-  $(`#in_country_days${day}`).addClass("Selected");
-});
+// var num = 2;
+// var day = 1;
+// $('#add_country').on('click', function() {
+//   $("#button_space").append('<div id="in_country_days'+num+'" class="testeReplicaBox"><div class="days_input"><input placeholder="Nome do país" class="form-control" name="budget[country][]"></input></div>   <div class="days_input"><input placeholder="Aeroporto de ída" class="form-control" name="budget[airportGo][]"></input><input placeholder="Aeroporto de chegada" class="form-control" name="budget[airportReturn][]"></input></div>   <div class="days_input"><input placeholder="data de ída" class="form-control" name="budget[goDate][]"></input><input placeholder="data de chegada" class="form-control" name="budget[returnDate][]"></input></div>   <div class="days_input"><input placeholder="Nome da cidade" class="form-control" name="budget[cities][]"></input></div>   </div>');
+//   day = num;
+//   num++;
+//   $(".testeReplicaBox").removeClass("Selected");
+//   $(`#in_country_days${day}`).addClass("Selected");ss
+// });
+//
+// $("#button_day_original").on("click", function(){
+//   var newDia = '<div class="days_input"><input placeholder="Nova cidade" class="form-control" name="budget[cities][]"></input></div>';
+//   $(`#in_country_days${day}`).append(newDia);
+// });
+//
+// $('#increasing').on('click', function() {
+//   if(day > 1){
+//     day--;
+//     $(".testeReplicaBox").removeClass("Selected");
+//     $(`#in_country_days${day}`).addClass("Selected");
+//   }
+// });
+//
+// $('#decreasing').on('click', function() {
+//   if(day < num-1){
+//     day++;
+//     $(".testeReplicaBox").removeClass("Selected");
+//     $(`#in_country_days${day}`).addClass("Selected");
+//   }
+// });
 
 $('.move').on('click', function() {
 });
@@ -64,6 +68,107 @@ $( "#add-voo" ).click(function() {
   $("#PageD_Col2").append('<div class="PageDColMidle"><h3> Voo '+numVoo+'</h3><br><br><input type="text" placeholder="Origem" name="flight[from]" class="date start-date"/><i class="fa fa-plane"></i><input type="text" placeholder="Destino" name="flight[destination]" class="date start-date"/><br><br><input type="date" placeholder="dd/mm/aa" name="flight[dateFrom]" class="date start-date"/><i class="fa fa-calendar"></i><input type="date" placeholder="dd/mm/aa" name="flight[dateDestination]" class="date start-date"/><br><br></div>');
   $("#PageD_Col3").append('<div class="PageDColRight"><h3> Valores '+numVoo+'</h3><br><br>Adulto:<input name="flight[tariffValueAdult]" type="text" id="num1" onblur="calcular1()" placeholder="Tarifa" class="valor"/><i class="fa fa-money"></i><input name="flight[taxValueAdult]" type="text" id="num2" onblur="calcular1()" placeholder="Tx Embarque" class="valor"/><i class="fa fa-money"></i><input name="flight[ravValueAdult]" type="text" id="num3" onblur="calcular1()" placeholder="Valor RAV" class="valor"/><span>&#61;</span><input name="flight[totalValueAdult]" type="text" id="result1" onblur="calcular1()" placeholder="Total" class="valor"/><span id="result"></span><br><br>CHD  :<input name="flight[tariffValueCHD]" type="text" id="num4" onblur="calcular2()" placeholder="Tarifa" class="valor"/><i class="fa fa-money"></i><input name="flight[taxValueCHD]" type="text" id="num5" onblur="calcular2()" placeholder="Tx Embarque" class="valor"/><i class="fa fa-money"></i><input name="flight[ravValueCHD]" type="text" id="num6" onblur="calcular2()" placeholder="Valor RAV" class="valor"/><span>&#61;</span><input name="flight[totalValueCHD]" type="text" id="result2" onblur="calcular2()" placeholder="Total" class="valor"/><span id="result"></span><br><br>INF   :<input name="flight[tariffValueInf]" type="text" id="num7" onblur="calcular3()" placeholder="Tarifa" class="valor"/><i class="fa fa-money"></i><input name="flight[taxValueInf]" type="text" id="num8" onblur="calcular3()" placeholder="Tx Embarque" class="valor"/><i class="fa fa-money"></i><input name="flight[ravValueInf]" type="text" id="num9" onblur="calcular3()" placeholder="Valor RAV" class="valor"/><span>&#61;</span><input name="flight[totalValueInf]" type="text" id="result3" onblur="calcular3()" placeholder="Total" class="valor"/><span id="result"></span><br><br>Total:<i class="fa fa-group" style="font-size:20px;color:grey"></i><input name="flight[finalValueInf]" type="text" id="result4" onblur="calcular4()" placeholder="Total" class="valor"/><br></div>');
   numVoo++;
+});
+
+var num = 2;
+var day = 1;
+
+var adtNum1 = 1;
+var adtNum2 = 2;
+var adtNum3 = 3;
+var adtRes = 1;
+
+var chdNum1 = 1;
+var chdNum2 = 2;
+var chdNum3 = 3;
+var chdRes = 1;
+
+var infNum1 = 1;
+var infNum2 = 2;
+var infNum3 = 3;
+var infRes = 1;
+
+var totalRes = 1;
+$('#add_country').on('click', function() {
+  adtNum1 = adtNum1 + 3;
+  adtNum2 = adtNum2 + 3;
+  adtNum3 = adtNum3 + 3;
+  adtRes++;
+
+  chdNum1 = chdNum1 + 3;
+  chdNum2 = chdNum2 + 3;
+  chdNum3 = chdNum3 + 3;
+  chdRes++;
+
+  infNum1 = infNum1 + 3;
+  infNum2 = infNum2 + 3;
+  infNum3 = infNum3 + 3;
+  infRes++;
+
+  totalRes++;
+  $("#button_space").append('<br><br><div class="header table-responsive" id="flight'+num+'"><table class="table table-striped table-hover" id="in_country_days'+num+'"><thead><tr><th>Origem</th><th>Destino</th><th>Saída</th><th>Chegada</th></tr></thead><tbody ><tr><th><input type="text" placeholder="Origem" name="flight[from]" class="form-control"/></th><th><input type="text" placeholder="Destino" name="flight[destination]" class="form-control"/></th><th><input type="date" placeholder="dd/mm/aa" name="flight[dateFrom]" class="form-control"/></th><th><input type="date" placeholder="dd/mm/aa" name="flight[dateDestination]" class="form-control"/></th></tr></tbody></table><div class="rowclass"><div class="rowclass"><h3>Adulto:</h3><input name="flight[tariffValueAdult]" type="text" id="adt'+adtNum1+'" onblur="calcularAdt()" placeholder="Tarifa" class="valor"/><i class="fa fa-money"></i><input name="flight[taxValueAdult]" type="text" id="adt'+adtNum2+'" onblur="calcularAdt()" placeholder="Tx Embarque" class="valor"/><i class="fa fa-money"></i><input name="flight[ravValueAdult]" type="text" id="adt'+adtNum3+'" onblur="calcularAdt()" placeholder="Valor RAV" class="valor"/><span>&#61;</span><input name="flight[totalValueAdult]" type="text" id="adtResult'+adtRes+'" onblur="calcularAdt()" placeholder="Total adt" class="valor"/><span id="result"></span></div><br><br><div class="rowclass"><h3>CHD  :</h3><input name="flight[tariffValueCHD]" type="text" id="chd'+chdNum1+'" onblur="calcularChd()" placeholder="Tarifa" class="valor"/><i class="fa fa-money"></i><input name="flight[taxValueCHD]" type="text" id="chd'+chdNum2+'" onblur="calcularChd()" placeholder="Tx Embarque" class="valor"/><i class="fa fa-money"></i><input name="flight[ravValueCHD]" type="text" id="chd'+chdNum3+'" onblur="calcularChd()" placeholder="Valor RAV" class="valor"/><span>&#61;</span><input name="flight[totalValueCHD]" type="text" id="chdResult'+chdRes+'" onblur="calcularChd()" placeholder="Total chd" class="valor"/><span id="result"></span></div><br><br><div class="rowclass"><h3>INF   :</h3><input name="flight[tariffValueInf]" type="text" id="inf'+infNum1+'" onblur="calcularInf()" placeholder="Tarifa" class="valor"/><i class="fa fa-money"></i><input name="flight[taxValueInf]" type="text" id="inf'+infNum2+'" onblur="calcularInf()" placeholder="Tx Embarque" class="valor"/><i class="fa fa-money"></i><input name="flight[ravValueInf]" type="text" id="inf'+infNum3+'" onblur="calcularInf()" placeholder="Valor RAV" class="valor"/><span>&#61;</span><input name="flight[totalValueInf]" type="text" id="infResult'+infRes+'" onblur="calcularInf()" placeholder="Total inf" class="valor"/><span id="result"></span></div><br><br><div class="rowclass"><i class="fa fa-group" style="font-size:20px;color:grey"></i><input name="flight[finalValueInf]" type="text" id="totalResult'+totalRes+'" onblur="total()" placeholder="Total" class="valor"/></div><br></div></div>');
+  day = num;
+  num++;
+  $(".Selected").removeClass("Selected");
+  $(`#flight${day}`).addClass("Selected");
+  console.log('criando numero');
+  console.log(day);
+});
+
+$("#button_day_original").on("click", function(){
+  var newDia = '<tr><th><input type="text" placeholder="Origem" name="flight[from]" class="form-control"/></th><th><input type="text" placeholder="Destino" name="flight[destination]" class="form-control"/></th><th><input type="date" placeholder="dd/mm/aa" name="flight[dateFrom]" class="form-control"/></th><th><input type="date" placeholder="dd/mm/aa" name="flight[dateDestination]" class="form-control"/></th></tr>';
+  $(`#in_country_days${day}`).append(newDia);
+  console.log(day);
+});
+
+$('#increasing').on('click', function() {
+  if(day > 1){
+    day--;
+
+    adtNum1 = adtNum1 - 3;
+    adtNum2 = adtNum2 - 3;
+    adtNum3 = adtNum3 - 3;
+    adtRes--;
+
+    chdNum1 = chdNum1 - 3;
+    chdNum2 = chdNum2 - 3;
+    chdNum3 = chdNum3 - 3;
+    chdRes--;
+
+    infNum1 = infNum1 - 3;
+    infNum2 = infNum2 - 3;
+    infNum3 = infNum3 - 3;
+    infRes--;
+
+    totalRes--;
+    $(".Selected").removeClass("Selected");
+    $(`#flight${day}`).addClass("Selected");
+  }
+});
+
+$('#decreasing').on('click', function() {
+  if(day < num-1){
+    day++;
+
+    adtNum1 = adtNum1 + 3;
+    adtNum2 = adtNum2 + 3;
+    adtNum3 = adtNum3 + 3;
+    adtRes++;
+
+    chdNum1 = chdNum1 + 3;
+    chdNum2 = chdNum2 + 3;
+    chdNum3 = chdNum3 + 3;
+    chdRes++;
+
+    infNum1 = infNum1 + 3;
+    infNum2 = infNum2 + 3;
+    infNum3 = infNum3 + 3;
+    infRes++;
+
+    totalRes++;
+    $(".Selected").removeClass("Selected");
+    $(`#flight${day}`).addClass("Selected");
+  }
 });
 
 // Page D - fim
