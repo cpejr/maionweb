4190e6f5e2bd8d4ba88ee90f8aa099dd55fb6502
João Pedro Rodrigues Trindade <jpr.trindade@gmail.com>
2019-11-05 09:55:52 -0200

    PageE New pronta

--- public/javascripts/operations.js
+++ public/javascripts/operations.js
@@ -404,37 +404,60 @@ $('#increasing').on('click', function(){
 /*! FIM OPERATIONS PAGE F !*/
 
 /*operations pageE*/
+var anumhotel = 1;
+var savedanumhotel = 1;
 
-function vHoteis1(){
-        var myBox1 = document.getElementById('vApto1').value;
-        var myBox2 = document.getElementById('nDiarias1').value;
-        var myBox3 = document.getElementById('nAptos1').value;
-        var result = document.getElementById('vTotal1');
-        var myResult = myBox1 * myBox2 * myBox3;
-          document.getElementById('vTotal1').value = myResult;
+var vApto1;
+var nDiarias1;
+var nAptos1;
+var somaHotel1 = 0;
 
+var vApto2;
+var nDiarias2;
+var nAptos2;
+var somaHotel2 = 0;
+
+var vApto3;
+var nDiarias3;
+var nAptos3;
+var somaHotel3 = 0;
+
+$("#add-hotel").click(function() {
+  savedanumhotel++;
+  anumhotel = savedanumhotel;
+});
+
+$('#prev_hotel').on('click', function() {
+  if (anumhotel > 1) {
+    anumhotel--;
+  }
+});
+
+$('#nxt_hotel').on('click', function() {
+  if (anumhotel < savedanumhotel) {
+    anumhotel++;
+  }
+});
+
+function calcHotel(){
+        vApto1 = document.getElementById(`vApto1${anumhotel}`).value;
+        nDiarias1 = document.getElementById(`nDiarias1${anumhotel}`).value;
+        nAptos1 = document.getElementById(`nAptos1${anumhotel}`).value;
+        somaHotel1 = vApto1 * nDiarias1 * nAptos1;
+        document.getElementById(`vTotal1${anumhotel}`).value = somaHotel1;
+
+        vApto2 = document.getElementById(`vApto2${anumhotel}`).value;
+        nDiarias2 = document.getElementById(`nDiarias2${anumhotel}`).value;
+        nAptos2 = document.getElementById(`nAptos2${anumhotel}`).value;
+        somaHotel2 = vApto2 * nDiarias2 * nAptos2;
+        document.getElementById(`vTotal2${anumhotel}`).value = somaHotel2;
+
+        vApto3 = document.getElementById(`vApto3${anumhotel}`).value;
+        nDiarias3 = document.getElementById(`nDiarias3${anumhotel}`).value;
+        nAptos3 = document.getElementById(`nAptos3${anumhotel}`).value;
+        somaHotel3 = vApto3 * nDiarias3 * nAptos3;
+        document.getElementById(`vTotal3${anumhotel}`).value = somaHotel3;
 }
-
-function vHoteis2(){
-        var myBox1 = document.getElementById('vApto2').value;
-        var myBox2 = document.getElementById('nDiarias2').value;
-        var myBox3 = document.getElementById('nAptos2').value;
-        var result = document.getElementById('vTotal2');
-        var myResult = myBox1 * myBox2 * myBox3;
-          document.getElementById('vTotal2').value = myResult;
-
-}
-
-function vHoteis3(){
-        var myBox1 = document.getElementById('vApto3').value;
-        var myBox2 = document.getElementById('nDiarias3').value;
-        var myBox3 = document.getElementById('nAptos3').value;
-        var result = document.getElementById('vTotal3');
-        var myResult = myBox1 * myBox2 * myBox3;
-          document.getElementById('vTotal3').value = myResult;
-
-}
-
 
 /*end operation pageE*/
 
